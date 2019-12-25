a52ff7f3de05e568d54a04142265363e07c1be67
markmliu <markmliu@umich.edu>
2019-12-25 14:16:32 -0800

    Add floor and letter class

---
+++ Assets/Scripts/Floor.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Floor : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    public Floor(int width,int height)
+    {
+
+    }
+
+    private void spawnLetter()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
