c48183360671fe62dedb1d5811de0c9ca185100c
Jordan <jminjie@umich.edu>
2019-12-28 23:23:09 -0500

    merge

--- Assets/Scripts/AIRacer.cs
+++ Assets/Scripts/AIRacer.cs
@@ -2,17 +2,25 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class AIRacer : MonoBehaviour
+public class AIRacer : Racer
 {
+    // Visible Game State that AI needs 
+
+
     // Start is called before the first frame update
-    void Start()
+    void Awake()
     {
-        
-    }
+		base.AwakeBase(
+			/*x=*/Floor.WIDTH / 3 * 2,
+			/*y=*/Floor.HEIGHT / 3,
+			/*otherRacerString=*/"Racer1",
+			new Color(255, 0, 221));
+		playerNum = 2;
+	}
 
     // Update is called once per frame
     void Update()
     {
-        
+        // try moving randomly
     }
 }
--- Assets/Scripts/GameHandler.cs
+++ Assets/Scripts/GameHandler.cs
@@ -16,6 +16,7 @@ public class GameHandler : MonoBehaviour
 
     private const int MAX_POINTS = 30;
 
+
     // Start is called before the first frame update
     void Start()
     {
